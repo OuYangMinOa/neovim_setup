d003ceeff6906b2e0696ea50d957c0ac22855cab
ouyang-company <wesley91345@gmail.com>
2025-05-15 02:54:48 +0000

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,6 @@ return {
     {require("plugins.harpoon") },
     {require("plugins.tree") },
     {require("plugins.indent")  },
-    {require("plugins.fold")},
     {require("plugins.style")},
 
 }                                                                                       

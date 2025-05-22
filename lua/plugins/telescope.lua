05b2249204a800e275519e1501a0fa04f5d13a04
ouyang-company <wesley91345@gmail.com>
2025-05-22 08:10:49 +0000

    update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,19 @@
 return {
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = {
+            {'nvim-lua/plenary.nvim'},{
+            "isak102/telescope-git-file-history.nvim",
+                dependencies = {
+                    "nvim-lua/plenary.nvim",
+                    "tpope/vim-fugitive"
+                }
+            }
+        },
         config =  function()
             local builtin = require('telescope.builtin')
+            require("telescope").load_extension("git_file_history")
+            vim.keymap.set('n', '<A-g>', '<cmd>Telescope git_file_history<CR>', { desc = 'Telescope git history' })
             vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
             vim.keymap.set('n', '<C-h>', function() require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")}) end )
             -- vim.keymap.set('n', '<C-F>', require("telescope").extensions.live_grep_args.live_grep_args, { noremap = true })
@@ -19,4 +29,5 @@ return {
         end
         -- <A-e> :Telescope undo<CR>
     }                            
-}                                                                                       
+}      
+                                                                                 

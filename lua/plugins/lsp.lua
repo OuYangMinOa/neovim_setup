27ff690b2b7560b5bc683f3911cc1717f9ef5d1d
ouyang-company <wesley91345@gmail.com>
2025-06-12 06:24:45 +0000

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -110,9 +110,20 @@ return {
         end
     },
     {
-        "linux-cultist/venv-selector.nvim",
-        dependencies = {"neovim/nvim-lspconfig","mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python",},
-        lazy   = false,
+       "linux-cultist/venv-selector.nvim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+            { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+        },
+        lazy = false,
         branch = "regexp", -- This is the regexp branch, use this for the new version
+        keys = {
+            { ",v", "<cmd>VenvSelect<cr>" },
+        },
+        ---@type venv-selector.Config
+        opts = {
+            -- Your settings go here
+        }, 
     }
 }                                                                                       
